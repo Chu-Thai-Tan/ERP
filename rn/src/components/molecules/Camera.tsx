e4765f6f88619f8c451262c2ef536da50c905408
Tan Chu <chuthaitan91098@gmail.com>
2023-11-22 14:40:06 +0700

    fix: comment

--- rn/src/components/molecules/Camera.tsx
+++ rn/src/components/molecules/Camera.tsx
@@ -9,16 +9,11 @@ type Props = {
 };
 
 export const Camera: FC<Props> = ({ onTakePhoto }) => {
-  // const camera = useRef<RTCView>(null);
   const [localStream, setLocalStream] = useState<MediaStream>();
   const startLocalStream = async () => {
     try {
       const mediaStream = await mediaDevices.getUserMedia({
         video: true,
-        // {
-        //   facingMode: 'user',
-        //   optional: videoSourceId ? [{sourceId: videoSourceId}] : [],
-        // },
       });
       setLocalStream(mediaStream);
       console.log(mediaStream.toURL());
@@ -38,31 +33,13 @@ export const Camera: FC<Props> = ({ onTakePhoto }) => {
     }
   };
   return (
-    <View
-    // style={styles.wrapper}
-    >
+    <View style={styles.wrapper}>
       {localStream && (
         <RTCView
-          // ref={camera}
-          // captureAudio={false}
           style={styles.camera}
-          // style={{flex: 1}}
           objectFit={'cover'}
           streamURL={localStream?.toURL() || ''}
-          // type={RNCamera.Constants.Type.front}
-          // androidCameraPermissionOptions={{
-          //   title: 'Permission to use camera',
-          //   message: 'We need your permission to use your camera',
-          //   buttonPositive: 'Ok',
-          //   buttonNegative: 'Cancel',
-          // }}>
-        >
-          {/* {!isScanning ? (
-        <Button title="Scan face" onPress={captureHandler} />
-      ) : (
-        <Button title="Stop scan" onPress={stopCaptureHandler} />
-      )} */}
-        </RTCView>
+        ></RTCView>
       )}
       <Button
         title="Stop scan"
@@ -77,7 +54,6 @@ const styles = StyleSheet.create({
   camera: {
     flex: 1,
     width: Dimensions.get('screen').width,
-    // flexDirection: 'column-reverse',
   },
   wrapper: {
     flex: 1,
