4d580cf0d0b616e2a43aad963709c236b4213c09
Tan Chu <chuthaitan91098@gmail.com>
2023-11-26 20:14:47 +0700

    update code

--- rn/src/polyfill.ts
+++ rn/src/polyfill.ts
@@ -1,32 +1,31 @@
-import {ViewPropTypes} from 'deprecated-react-native-prop-types'
-import {Buffer} from 'buffer'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import * as RN from 'react-native'
-import RN2 from 'react-native'
-
+import { ViewPropTypes } from 'deprecated-react-native-prop-types';
+import { Buffer } from 'buffer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as RN from 'react-native';
+import RN2 from 'react-native';
 
 Object.defineProperty(RN, 'ViewPropTypes', {
   get() {
-    return ViewPropTypes
-  }
-})
+    return ViewPropTypes;
+  },
+});
 
 Object.defineProperty(RN2, 'ViewPropTypes', {
   get() {
-    return ViewPropTypes
-  }
-})
+    return ViewPropTypes;
+  },
+});
 
 Object.defineProperty(RN, 'AsyncStorage', {
   get() {
-    return AsyncStorage
-  }
-})
+    return AsyncStorage;
+  },
+});
 
 Object.defineProperty(RN2, 'AsyncStorage', {
   get() {
-    return AsyncStorage
-  }
-})
+    return AsyncStorage;
+  },
+});
 
-global.Buffer = Buffer
+global.Buffer = Buffer;
