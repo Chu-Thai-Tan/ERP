1f05e91499ff89cccfadac572ee9a2e0bf8e2ed5
TriNguyen <nguyeminhtrii@gmail.com>
2023-12-01 15:23:16 +0700

    fix

---
+++ src/components/molecules/Camera.tsx
@@ -0,0 +1,61 @@
+import { FC, useRef } from 'react';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import { Button } from '../atoms/Button';
+import { RNCamera } from 'react-native-camera';
+import RNFS from 'react-native-fs';
+
+type Props = {
+  onTakePhoto: (data: string) => void;
+};
+
+export const Camera: FC<Props> = ({ onTakePhoto }) => {
+  const camera = useRef<RNCamera>(null);
+
+  const takePictureHandler = async () => {
+    if (camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await camera.current
+        ?.takePictureAsync(options)
+        .catch(e => console.log(JSON.stringify(e)));
+      console.log(data);
+      const base64 = await getBase64(data?.uri ?? '').catch(e =>
+        console.log(e),
+      );
+      console.log(base64);
+      onTakePhoto(base64 ?? '');
+    }
+  };
+  const getBase64 = async (imageUri: string) => {
+    const filepath = imageUri.split('//')[1];
+    const imageUriBase64 = await RNFS.readFile(filepath, 'base64');
+    return `${imageUriBase64}`;
+  };
+
+  return (
+    <View style={styles.wrapper}>
+      <RNCamera
+        ref={camera}
+        style={styles.camera}
+        type={RNCamera.Constants.Type.front}
+      >
+        <Button title="Take picture" onPress={takePictureHandler} />
+      </RNCamera>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  camera: {
+    flex: 1,
+    width: Dimensions.get('screen').width,
+    flexDirection: 'column-reverse',
+    alignItems: 'center',
+  },
+  wrapper: {
+    flex: 1,
+    width: Dimensions.get('screen').width,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
--- src/screens/CheckIn.tsx
+++ src/screens/CheckIn.tsx
@@ -1,48 +1,20 @@
-import { FC, useRef } from 'react';
-import { Dimensions, StyleSheet, View } from 'react-native';
-import { Button } from '../components/atoms/Button';
-import { RNCamera } from 'react-native-camera';
-import RNFS from 'react-native-fs';
+import { FC } from 'react';
+import { Dimensions, StyleSheet, View, Text } from 'react-native';
+import { Camera } from '../components/molecules/Camera';
+import { RouteProp } from '@react-navigation/native';
 
 type Props = {
   onTakePhoto: (data: string) => void;
-  navigation: any;
+  route: RouteProp<{}>;
+  //  navigation: any;
 };
-const CheckIn: FC<Props> = ({ navigation, route }: any) => {
-  const camera = useRef<RNCamera>(null);
+const CheckIn: FC<Props> = ({ route }) => {
   const { onTakePhoto } = route.params;
-  const takePictureHandler = async () => {
-    if (camera) {
-      const options = { quality: 0.5, base64: true };
-      const data = await camera.current
-        ?.takePictureAsync(options)
-        .catch(e => console.log(JSON.stringify(e)));
-      console.log(data);
-      const base64 = await getBase64(data?.uri ?? '').catch(e =>
-        console.log(e),
-      );
-      console.log(base64);
-      onTakePhoto(base64 ?? '');
-    }
-  };
-  const getBase64 = async (imageUri: string) => {
-    const filepath = imageUri.split('//')[1];
-    const imageUriBase64 = await RNFS.readFile(filepath, 'base64');
-    return `${imageUriBase64}`;
-  };
-  const backToHome = () => {
-    navigation.navigate('Home');
-  };
 
   return (
     <View style={styles.wrapper}>
-      <RNCamera
-        ref={camera}
-        style={styles.camera}
-        type={RNCamera.Constants.Type.front}
-      >
-        <Button title="Take picture" onPress={takePictureHandler} />
-      </RNCamera>
+      <Camera onTakePhoto={onTakePhoto} />
+      {/*<Text>abc</Text>*/}
     </View>
   );
 };
@@ -50,12 +22,6 @@ const CheckIn: FC<Props> = ({ navigation, route }: any) => {
 export default CheckIn;
 
 const styles = StyleSheet.create({
-  camera: {
-    flex: 1,
-    width: Dimensions.get('screen').width,
-    flexDirection: 'column-reverse',
-    alignItems: 'center',
-  },
   wrapper: {
     flex: 1,
     width: Dimensions.get('screen').width,
