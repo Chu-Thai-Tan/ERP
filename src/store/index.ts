2b66607ce9584ac493f82ec96887e3c819d843b7
Tan Chu <chuthaitan91098@gmail.com>
2023-12-01 17:54:38 +0700

    feat: store selector setup

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import { authReducer } from './authSlice';
 import { checkInReducer } from './checkInSlice';
-import { useDispatch } from 'react-redux';
+import { TypedUseSelectorHook, useDispatch } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import { ThunkDispatch } from 'redux-thunk';
+import { useSelector } from 'react-redux';
 
 export const store = configureStore({
   reducer: {
@@ -17,6 +18,7 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export type AppThunkDispatch = ThunkDispatch<RootState, any, Action<any>>;
 export const useAppDispatch = () => useDispatch<AppThunkDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
