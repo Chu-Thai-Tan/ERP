ca4677e43606ea33774c9a15f7fb22eb65c157eb
Duy Phann <phanthanhduy.jin@gmail.com>
2023-12-06 16:33:17 +0700

    update

---
+++ src/components/molecules/Calendar.tsx
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { ExpandableCalendar, CalendarProvider } from 'react-native-calendars';
+import moment from 'moment'
+
+const CustomWeekday = ({ day }: { day: any }) => {
+  console.log('#Duy Phan console', day);
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 16, fontWeight: 'bold', color: 'blue' }}>
+        {day.children}
+      </Text>
+    </View>
+  );
+};
+
+export const Calendar = () => {
+ 
+  const [date, setDate] = useState<string>(moment(new Date()).format('YYYY-MM-DD'));
+
+  return (
+    <View style={styles.container}>
+      <CalendarProvider date={date}>
+        <ExpandableCalendar
+          style={styles.knobContainer}
+          allowShadow
+          horizontal
+          hideArrows
+          firstDay={1}
+          disablePan={true}
+          calendarStyle={styles.calendar}
+          onDayPress={d => setDate(d.dateString)}
+          //   dayComponent={(d) => <CustomWeekday  day={d}/>}
+          //   renderHeader={date => <Text>11111</Text>}
+          markedDates={{
+            [date]: {
+              selected: true,
+              marked: false,
+              customContainerStyle: styles.knob,
+              theme: {
+                textDayStyle: {
+                  borderRadius: 0,
+                },
+              },
+              customStyles: {
+                container: styles.knob,
+                text: {
+                  color: 'white',
+                  fontWeight: 'bold',
+                },
+              },
+            },
+          }}
+          markingType="custom"
+        />
+      </CalendarProvider>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: '100%',
+    position: 'relative',
+  },
+  knobContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    height: 24,
+    top: 120,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  knob: {
+    width: 40,
+    height: 30,
+    borderRadius: 8,
+    backgroundColor: 'red',
+  },
+  calendar: {
+    backgroundColor: 'transparent',
+  },
+  markedContainer: {
+    borderRadius: 0,
+    backgroundColor: 'yellow',
+  },
+});
