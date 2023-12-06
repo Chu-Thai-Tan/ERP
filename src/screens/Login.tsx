ba15d37dde400d266ac6250233c143b63015a3eb
TriNguyen <nguyeminhtrii@gmail.com>
2023-12-06 16:56:27 +0700

    feat UI login

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,18 +1,56 @@
-import { Text, TouchableOpacity } from 'react-native';
-import { Button } from '../components/atoms/Button';
+import { TouchableOpacity } from 'react-native';
+import { Stack, Text, Input, styled, Button, Image } from 'tamagui';
+import Logo from '../assets/image/Logo.png';
 
 export function Login({ navigation }: any) {
   const handleOnPress = () => {
     navigation.navigate('Register');
   };
+
+  const Wrapper = styled(Stack, {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#CCCCFF',
+    height: '100%',
+  });
+
+  const CustomInput = styled(Input, {
+    size: '$4',
+    width: '70%',
+    marginTop: 10,
+  });
+
+  const CustomButton = styled(Button, {
+    size: '$4',
+    width: '70%',
+    marginTop: 10,
+    backgroundColor: '#DA70D6',
+  });
+
+  const CustomText = styled(Text, {
+    marginTop: 10,
+    color: 'red',
+  });
+
+  const CustomImage = styled(Image, {
+    marginBottom: 60,
+    source: {
+      width: '59%',
+      height: 38,
+      uri: Logo,
+    },
+  });
+
   return (
-    <>
-      <Text>Email: nguyeminhtrii@gmail.com</Text>
-      <Text>Password: ***********</Text>
-      <Button title="Login" />
+    <Wrapper>
+      <CustomImage alt="Logo" />
+
+      <CustomInput placeholder="Email" />
+      <CustomInput placeholder="Password" />
+      <CustomButton>Login</CustomButton>
       <TouchableOpacity onPress={handleOnPress}>
-        <Text style={{ color: 'red' }}>Register</Text>
+        <CustomText>Register</CustomText>
       </TouchableOpacity>
-    </>
+    </Wrapper>
   );
 }
--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,5 +1,47 @@
-import { Text } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+//import { Button } from '../components/atoms/Button';
+import { Stack, Text, Input, styled, Button } from 'tamagui';
 
-export function Register() {
-  return <Text>Register</Text>;
+export function Register({ navigation }: any) {
+  const handleOnPress = () => {
+    navigation.navigate('Login');
+  };
+
+  const Wrapper = styled(Stack, {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#CCCCFF',
+    height: '100%',
+  });
+
+  const CustomInput = styled(Input, {
+    size: '$4',
+    width: '70%',
+    marginTop: 10,
+  });
+
+  const CustomButton = styled(Button, {
+    size: '$4',
+    width: '70%',
+    marginTop: 10,
+    backgroundColor: '#DA70D6',
+  });
+
+  const CustomText = styled(Text, {
+    marginTop: 10,
+    color: 'red',
+  });
+
+  return (
+    <Wrapper>
+      <CustomInput placeholder="Name" />
+      <CustomInput placeholder="Email" />
+      <CustomInput placeholder="Password" />
+      <CustomInput placeholder="Confirm Password" />
+      <CustomButton>Register</CustomButton>
+      <TouchableOpacity onPress={handleOnPress}>
+        <CustomText>Login</CustomText>
+      </TouchableOpacity>
+    </Wrapper>
+  );
 }
