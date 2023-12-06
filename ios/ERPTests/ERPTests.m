9289ffc2da188cb24912e3de1aa1ecb5602ed034
Duy Phann <phanthanhduy.jin@gmail.com>
2023-12-06 11:43:53 +0700

    update

--- ios/ERPTests/ERPTests.m
+++ ios/ERPTests/ERPTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rnTests : XCTestCase
+@interface ERPTests : XCTestCase
 
 @end
 
-@implementation rnTests
+@implementation ERPTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
