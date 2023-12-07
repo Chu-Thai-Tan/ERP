2b1ce6a5558370c9fb9a31bbb1b414706c83e06d
Tan Chu <chuthaitan91098@gmail.com>
2023-12-07 14:54:34 +0700

    fix: navigation

--- src/routes/RootNavigation.tsx
+++ src/routes/RootNavigation.tsx
@@ -4,12 +4,18 @@ import { HomeStack } from './homeStack';
 import { AuthStack } from './authStack';
 import { useSelector } from 'react-redux';
 import { authStatus } from '../store/auth/selectors';
+import { isReadyRef, navigationRef } from '../utils/navigateService';
 
 export function RootNavigation() {
   const isLogin = useSelector(authStatus);
 
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() => {
+        isReadyRef.current = true;
+      }}
+    >
       {isLogin ? <HomeStack /> : <AuthStack />}
     </NavigationContainer>
   );
