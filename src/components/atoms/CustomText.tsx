7a44aaa1fd32a76b88f5c659d37f34eca057caa3
Tan Chu <chuthaitan91098@gmail.com>
2023-12-08 11:51:35 +0700

    feat: add font family to CustomText

--- src/components/atoms/CustomText.tsx
+++ src/components/atoms/CustomText.tsx
@@ -4,6 +4,7 @@ export const CustomText: React.FC<TextProps> = props => {
   const CustomText = styled(Text, {
     color: '#800080',
     fontWeight: 'bold',
+    ff: '$body',
   });
   return <CustomText {...props} />;
 };
