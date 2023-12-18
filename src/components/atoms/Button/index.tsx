a7fccfe174d738462731ebc0de36c579ba9e4248
Tan Chu <chuthaitan91098@gmail.com>
2023-12-18 13:46:06 +0700

    fix: warning forwardRef

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,6 +1,9 @@
-import { ButtonProps } from 'tamagui';
+import { ButtonProps, TamaguiElement } from 'tamagui';
 import { CustomButton } from './styles';
+import { Ref, forwardRef } from 'react';
 
-export const Button: React.FC<ButtonProps> = props => {
-  return <CustomButton {...props} />;
-};
+export const Button = forwardRef(
+  (props: ButtonProps, ref: Ref<TamaguiElement>) => {
+    return <CustomButton {...props} ref={ref} />;
+  },
+);
